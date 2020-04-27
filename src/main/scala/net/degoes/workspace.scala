7fd2d36d87cc7eb8ba81d6c03ec48fe42ee9116d
John A. De Goes <john@degoes.net>
2020-04-27 07:15:08 -0400

    Add workspace

---
+++ src/main/scala/net/degoes/workspace.scala
@@ -0,0 +1,18 @@
+package net.degoes
+
+object workspace {
+  //
+  // Please join and say hello:
+  //
+  // CHAT ROOM:  https://gitter.im/jdegoes/functional-scala
+  //
+  // Please git clone and build:
+  //
+  // REPOSITORY: https://github.com/jdegoes/functional-design
+  //
+  // Daily Schedule:
+  //
+  //    START : 
+  //    LUNCH : 
+  //    END   : 
+}
