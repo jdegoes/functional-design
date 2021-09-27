be725b8a779749a5e2ef3487fb0866e864b99db8
John A. De Goes <john@degoes.net>
2021-09-27 10:44:26 -0400

    More updates

--- src/main/scala/net/degoes/00_intro.scala
+++ src/main/scala/net/degoes/00_intro.scala
@@ -108,4 +108,109 @@ object tour {
     }
   }
 
+  object parsers {
+    type Input = String
+
+    /**
+     * The start of a parser that parses emails.
+     */
+    lazy val email = username + Parser.char('@') + server
+
+    lazy val letterOrDigit =
+      Parser.anyChar.suchThat(c => c.isLetter || c.isDigit, "Expected a letter or digit")
+
+    lazy val username = letterOrDigit.repeatedly
+
+    lazy val tld = Parser.string("com") | Parser.string("net")
+
+    lazy val server = letterOrDigit.repeatedly + Parser.char('@').string + tld
+
+    final case class Parser[+E, +A](parse: Input => Either[E, (Input, A)]) { self =>
+      def flatMap[E1 >: E, B](f: A => Parser[E1, B]): Parser[E1, B] =
+        Parser { input =>
+          parse(input).flatMap {
+            case (input, a) => f(a).parse(input)
+          }
+        }
+
+      def map[B](f: A => B): Parser[E, B] = self.flatMap(a => Parser.succeed(f(a)))
+
+      def +[E1 >: E, B](that: => Parser[E1, B])(implicit ev1: StringLike[A], ev2: StringLike[B]): Parser[E1, String] =
+        self.string.zipWith(that.string)(_ + _)
+
+      def ~[E1 >: E, B](that: => Parser[E1, B]): Parser[E1, (A, B)] = self.zip(that)
+
+      def zip[E1 >: E, B](that: => Parser[E1, B]): Parser[E1, (A, B)] =
+        for {
+          a <- self
+          b <- that
+        } yield (a, b)
+
+      def zipWith[E1 >: E, B, C](that: => Parser[E1, B])(f: (A, B) => C): Parser[E1, C] =
+        (self zip that).map(f.tupled)
+
+      def *>[E1 >: E, B](that: => Parser[E1, B]): Parser[E1, B] = (self zip that).map(_._2)
+
+      def <*[E1 >: E](that: => Parser[E1, Any]): Parser[E1, A] = (self zip that).map(_._1)
+
+      def |[E1 >: E, A1 >: A](that: => Parser[E1, A1]): Parser[E1, A1] = self.orElse(that)
+
+      def orElse[E1 >: E, A1 >: A](that: => Parser[E1, A1]): Parser[E1, A1] =
+        Parser { input =>
+          self.parse(input) match {
+            case Left(_) => that.parse(input)
+            case other   => other
+          }
+        }
+
+      def repeatedly: Parser[E, Vector[A]] =
+        self.map(Vector(_)).zipWith(repeatedly)(_ ++ _).orElse(Parser.succeed(Vector()))
+
+      def suchThat[E1 >: E](f: A => Boolean, e: E1): Parser[E1, A] =
+        for {
+          a <- self
+          _ <- if (f(a)) Parser.succeed(()) else Parser.fail(e)
+        } yield a
+
+      def string(implicit ev: StringLike[A]): Parser[E, String] =
+        self.map(ev).map(_.toString)
+    }
+    object Parser {
+      def succeed[A](a: => A): Parser[Nothing, A] = Parser(input => Right(input -> a))
+
+      def fail[E](e: => E): Parser[E, Nothing] = Parser(_ => Left(e))
+
+      def char(c: Char): Parser[String, Char] =
+        for {
+          char <- anyChar
+          _    <- if (char == c) Parser.succeed(()) else Parser.fail("Expected " + c)
+        } yield char
+
+      val anyChar: Parser[String, Char] =
+        Parser { input =>
+          if (input.length == 0) Left("Need More input!")
+          else Right(input.drop(1) -> input.charAt(0))
+        }
+
+      def string(s: String): Parser[String, String] =
+        Parser { input =>
+          if (input.startsWith(s)) Right(input.drop(s.length) -> s)
+          else Left(s"Expected ${s} but found " + input.take(s.length) + "...")
+        }
+    }
+
+    trait StringLike[-A] extends (A => String) {
+      final def apply(a: A): String = makeString(a)
+      def makeString(a: A): String
+    }
+    object StringLike {
+      implicit val stringLikeChar: StringLike[Char] = _.toString
+
+      implicit def stringLikeVector[A](implicit sl: StringLike[A]): StringLike[Vector[A]] =
+        (vector: Vector[A]) => vector.map(sl.makeString(_)).mkString("")
+
+      implicit val stringLikeString: StringLike[String] = identity[String](_)
+    }
+  }
+
 }
